896b030c00979c1f902bdfab88b3c25607309ed2
ChrisRDavila <crossdavila@gmail.com>
2023-05-07 19:45:26 -0700

    boiler plate for banner.cs added

--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -1,6 +1,6 @@
-namespace ProjectName.Models
+namespace ProjectName.Models //update namespace here
 {
-  public class ClassName
+  public class ClassName //create model class here
   {
     // Business Logic goes here.
   }
---
+++ ProjectName/UserInterfaceModels/ClassName.cs
@@ -0,0 +1,17 @@
+namespace ProjectName.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @"";
+  }
+
+  class GoodbyeBanner
+  {
+    public static string Goodbye { get;} = @"";
+  }
+
+  class SubBanner
+  {
+    public static string Other { get; } = @"";
+  }
+}
